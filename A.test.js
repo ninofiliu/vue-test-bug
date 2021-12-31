b852816f575da8734e0c20b24046c656c41d11da
Nino Filiu <nino.filiu@gmail.com>
2021-12-31 17:40:23 +0100

    use dynamic-import-node

--- A.test.js
+++ A.test.js
@@ -2,8 +2,7 @@ import { shallowMount } from '@vue/test-utils';
 import A from './A.vue';
 import B from './B.vue';
 
-it('should work', async () => {
+it('should work', () => {
   const wrapper = shallowMount(A, { stubs: { B }});
-  await wrapper.vm.$nextTick()
   wrapper.getComponent(B);
 });
